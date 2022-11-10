e8442812bdfeadce73900f9b2ac1769ae48a7308
paopaochang0218 <1469221864@qq.com>
2022-11-10 16:16:51 +0800

    自述文档

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# reactbuild
-react脚手架
+# 一级标题
+
+## 二级标题
+
+![皮卡丘](http://wz321.cp1j07.cnaaa3.com/pkq.png)
+
+[百度](http://www.baidu.com)
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import Style from './css/style'
 export default class App extends Component {
   render() {
     return (
       <div>
         <h3>App组件</h3>
+        <div className={Style.special}>
+          <label htmlFor="userName">用户名</label>
+        <input type="text" id='userName' /><br />
+        <label htmlFor="password">密码</label>
+        <input type="text" id='password' />
+        </div>
       </div>
     )
   }
